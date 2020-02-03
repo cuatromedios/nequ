97b332898967efdef8d8147b08fb71e7a99cb5e3
Roberto Arosemena <arosemenae@gmail.com>
2020-02-03 12:57:52 -0600

    Entity import/export in user's module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,9 @@
 import {Module} from '@nestjs/common'
-import {UserController} from './user.controller'
-import {UserService} from './user.service'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {User} from './user.entity'
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule]
 })
 export class UserModule {}
