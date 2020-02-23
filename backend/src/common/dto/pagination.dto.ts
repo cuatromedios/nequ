563552c2b6e0876ba490de498ae7964b1945ec15
Roberto Arosemena <arosemenae@gmail.com>
2020-02-23 14:10:42 -0600

    Added the pagination dto

---
+++ backend/src/common/dto/pagination.dto.ts
@@ -0,0 +1,23 @@
+import {IsInt, IsOptional, Min} from 'class-validator'
+import {ApiProperty} from '@nestjs/swagger'
+import {Expose, Transform} from 'class-transformer'
+
+export class PaginationDto {
+  @Transform(n => parseInt(n))
+  @IsOptional() @IsInt() @Min(0)
+  @ApiProperty({example: 25, required: false})
+  take: number
+  @Transform(n => parseInt(n))
+  @IsInt() @IsInt() @Min(0)
+  @ApiProperty({example: '0', required: false})
+  skip: number
+
+  @Expose()
+  get params() {
+    if (!this.take) {
+      this.take = parseInt(process.env.PAGINATION_DEFAULT_N) || 25
+    }
+    if (!this.skip) this.skip = 0
+    return {take: this.take, skip: this.skip}
+  }
+}
