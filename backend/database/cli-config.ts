32818333da41436b2440b86b9b5fe84ebc058dcf
Roberto <roberto@villavanilla.com>
2020-06-22 20:13:55 -0500

    - cli-config Is now importing the base ormconfig and adding the extra configuration for migrations. With this we are trying to minimize the code duplication by mirroring the shared configurations.

--- backend/database/cli-config.ts
+++ backend/database/cli-config.ts
@@ -1,14 +1,10 @@
 import {ConnectionOptions} from 'typeorm'
+import * as ormConfig from './config'
+
+let conf = ormConfig()
 
 const config: ConnectionOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [],
-  synchronize: false,
+  ...conf,
   migrations: ['database/migrations/*.ts'],
   cli: {migrationsDir: 'database/migrations'}
 }
